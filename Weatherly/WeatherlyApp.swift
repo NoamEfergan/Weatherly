0dd588c422decc55b39c11044499d89840b32ddd
Noam Efergan <noamifergan@gmail.com>
2021-05-01 09:48:23 +0300

    Initial Commit

---
+++ Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by Noam Efergan on 01/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
